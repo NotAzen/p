3703c1de9756ec827495a96dfa2c20c29cb3410a
SURFACEPRO\Dylan <azenosnothere@gmail.com>
2025-10-22 22:53:20 -0700

    eightgon font (still needs a font atlas) + HUD handler wip (try to hook bars to script or smth idk)

--- Assets/Scripts/StatisticPercentage.cs
+++ Assets/Scripts/StatisticPercentage.cs
@@ -5,9 +5,40 @@ public class StatisticPercentage : MonoBehaviour
     // --------------------------------------------------------------------------------- //
     // PUBLIC VARIABLES
 
+    [SerializeField] GameObject Player;
+
     [Header("Statistics")]
-    [SerializeField] GameObject HPBar;
-    [SerializeField] GameObject StaminaBar;
+    [SerializeField] GameObject HPStatsUI;
+    [SerializeField] GameObject StaminaStatsUI;
+
+    // class for handling UI updates related to the statistic it tracks
+    class UIHandler
+    {
+        // public variables for the UI GameObject and the game statistic value
+        public GameObject UIObject;
+        public TextMesh UIText;
+        public float GameStatistic;
+
+        // constructor that takes a UI GameObject and a game statistic value (class initialization)
+        public UIHandler(GameObject uiObject, float gameStatistic)
+        {
+            UIObject = uiObject;
+            GameStatistic = gameStatistic;
+        }
+
+        // ----------------------------------------------------------------------------------- //
+        // PUBLIC METHODS
+
+        public void UpdateUI()
+        {
+            Debug.Log("Updating UI...");
+        }
+
+        public void UpdateText()
+        {
+            Debug.Log("Updating Text...");
+        }
+    }
 
     // --------------------------------------------------------------------------------- //
     // PRIVATE VARIABLES
@@ -18,8 +49,10 @@ public class StatisticPercentage : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        FullHP = HPBar.transform;
-        FullStamina = StaminaBar.transform;
+        //FullHP = HPBar.transform;
+        //FullStamina = StaminaBar.transform;
+
+        
     }
 
     // Update is called once per frame
